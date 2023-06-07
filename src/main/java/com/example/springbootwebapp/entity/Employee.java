d3b287e49671ef5c0a23381350ff50b63dff3dc3
Andualem22 <andualemteshome459@gmail.com>
2023-06-07 08:40:18 -0500

    Initial commit

---
+++ src/main/java/com/example/springbootwebapp/entity/Employee.java
@@ -0,0 +1,57 @@
+package com.example.springbootwebapp.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class Employee {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    
+	private Long id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String department;
+	private String email;
+	private String name;
+	
+}
+
